bff509605fcbc828d0b77a98eecb8d008160219e
chegior <chegiorca@hotmail.com>
2020-08-23 14:51:21 -0500

    boiler plate ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/code.ts',
+    // the entry code would be on the folder below
+    entry: './source/code.ts',
     devtool: 'inline-source-map',
     module:{
         rules:[
